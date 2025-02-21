483f341b6e34b1f4c73131a876c54c67549560ac
vladiuse <vladiuse@gmail.com>
2025-02-21 18:40:23 +0300

    add urls

--- engCards/engCards/urls.py
+++ engCards/engCards/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('vocabulary.urls')),
+    path('card-trainer/', include('card_trainer.urls')),
 ]
