130668b152d5e4129756617e1679c4329705db94
vladiuse <vladiuse@gmail.com>
2025-02-25 21:36:43 +0300

    add funct to enum

--- engCards/card_trainer/types.py
+++ engCards/card_trainer/types.py
@@ -6,6 +6,17 @@ class LangDirection(Enum):
     RU_EN = 'ru_en'
     EN_RU = 'en_ru'
 
+    def reverse(self):
+        if self == LangDirection.RU_EN:
+            return LangDirection.EN_RU
+        return LangDirection.RU_EN
+
+    @classmethod
+    @property
+    def choices(cls):
+        return [item.value for item in cls]
+
+
 class RuEnPair(Protocol):
     id: int | str
     ru: str
