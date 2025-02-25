7d1b526f504d96e8e5a8ee8ec89853f7d7acec90
vladiuse <vladiuse@gmail.com>
2025-02-25 21:36:55 +0300

    add serializers.py

--- engCards/card_trainer/serializers.py
+++ engCards/card_trainer/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from .types import LangDirection
 
+
+class TranslationDirectionSerializer(serializers.Serializer):
+    direction = serializers.ChoiceField(choices=LangDirection.choices)
+
+    def create(self, validated_data):
+        return LangDirection(validated_data['direction'])
+
 class CardWordSerializer(serializers.Serializer):
     word_id = serializers.CharField()
     word = serializers.CharField()
@@ -9,4 +16,4 @@ class CardWordSerializer(serializers.Serializer):
 class CardSerializer(serializers.Serializer):
     target = CardWordSerializer()
     answers = CardWordSerializer(many=True)
-    lang_direction = serializers.CharField(choices=[e.value for e in LangDirection])
+    lang_direction = serializers.ChoiceField(choices=LangDirection.choices)
