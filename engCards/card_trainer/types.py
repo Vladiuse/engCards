0bde827fc58fe0f35a274930121f4e9b75b5a49d
vladiuse <vladiuse@gmail.com>
2025-02-26 23:13:19 +0300

    add type hints

--- engCards/card_trainer/types.py
+++ engCards/card_trainer/types.py
@@ -1,23 +1,22 @@
 from enum import Enum
 from typing import Protocol
 
-
 class LangDirection(Enum):
     RU_EN = 'ru_en'
     EN_RU = 'en_ru'
 
-    def reverse(self):
+    def reverse(self) -> 'LangDirection':
         if self == LangDirection.RU_EN:
             return LangDirection.EN_RU
         return LangDirection.RU_EN
 
     @classmethod
     @property
-    def choices(cls):
+    def choices(cls) -> list[str]:
         return [item.value for item in cls]
 
 
 class RuEnPair(Protocol):
-    id: int | str
+    pk: int | str
     ru: str
     en: str
