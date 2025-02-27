89b02f11f649963c0335b93fa790a40db44a0411
vladiuse <vladiuse@gmail.com>
2025-02-27 17:18:26 +0300

    fix validator

--- engCards/vocabulary/validators.py
+++ engCards/vocabulary/validators.py
@@ -11,10 +11,16 @@ def remove_not_alpha_chars(string: str) -> str:
 
 
 def eng_chars_validator(value) -> None:
+    value = remove_not_alpha_chars(string=value)
+    if value == '':
+        raise ValidationError('Слово должно быть из английских букв')
     if not re.match(ENG_CHARS_REGEX, value):
         raise ValidationError('Должны быть только английские буквы и символы')
 
 
 def ru_chars_validator(value) -> None:
+    value = remove_not_alpha_chars(string=value)
+    if value == '':
+        raise ValidationError('Слово должно быть из русских букв')
     if not re.match(RU_CHARS_REGEX, value):
         raise ValidationError('Должны быть только русские буквы и символы')
