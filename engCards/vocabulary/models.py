8e9d02f0bca76f7047fb6b5b94a7e95d23de00ac
vladiuse <vladiuse@gmail.com>
2025-02-27 14:11:08 +0300

    add new fields

--- engCards/vocabulary/models.py
+++ engCards/vocabulary/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from .fields import EngCharField, RuCharField
+
+
 class EnglishLevel(models.Model):
     id = models.CharField(max_length=2, primary_key=True)
     name = models.CharField(max_length=20)
@@ -10,8 +13,8 @@ class EnglishLevel(models.Model):
 
 class DefaultWord(models.Model):
     number_in_dict = models.PositiveIntegerField()
-    en = models.CharField(max_length=100)
-    ru = models.CharField(max_length=100)
+    en = EngCharField(max_length=100)
+    ru = RuCharField(max_length=100)
 
 
 class WordPair(models.Model):
@@ -23,8 +26,8 @@ class WordPair(models.Model):
         (LEARNING, 'Изучаю'),
         (POSTPONED, 'Отложено'),
     )
-    en = models.CharField(max_length=100)
-    ru = models.CharField(max_length=100)
+    en = EngCharField(max_length=100)
+    ru = RuCharField(max_length=100)
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='words', related_query_name='word')
     created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=STATUSES, default=LEARNING)
@@ -35,11 +38,17 @@ class WordPair(models.Model):
     def __str__(self):
         return f'{self.en} - {self.ru}'
 
+
 class Sentence(models.Model):
     words = models.ManyToManyField(to=WordPair, related_name='sentences', related_query_name='sentence')
-    en = models.CharField(max_length=255)
-    ru = models.CharField(max_length=255)
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='sentences', related_query_name='sentence')
+    en = EngCharField(max_length=255)
+    ru = RuCharField(max_length=255)
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='sentences',
+        related_query_name='sentence',
+    )
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
