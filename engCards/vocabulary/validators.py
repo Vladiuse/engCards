639d486088b66ca1905eef15673436488dfda2cd
vladiuse <vladiuse@gmail.com>
2025-02-27 11:53:36 +0300

    add validators

---
+++ engCards/vocabulary/validators.py
@@ -0,0 +1,20 @@
+import re
+
+from django.core.validators import ValidationError
+
+ENG_CHARS_REGEX = r'^[A-Za-z]+$'
+RU_CHARS_REGEX = r'^[А-Яа-яЁё]+$'
+
+
+def remove_not_alpha_chars(string: str) -> str:
+    return ''.join(char for char in string if char.isalpha())
+
+
+def eng_chars_validator(value) -> None:
+    if not re.match(ENG_CHARS_REGEX, value):
+        raise ValidationError('Должны быть только английские буквы и символы')
+
+
+def ru_chars_validator(value) -> None:
+    if not re.match(RU_CHARS_REGEX, value):
+        raise ValidationError('Должны быть только русские буквы и символы')
