cf483c773514049be27d2a3b8ef6775fb3e0c0e1
vladiuse <vladiuse@gmail.com>
2025-02-28 14:09:58 +0300

    add static

--- engCards/engCards/urls.py
+++ engCards/engCards/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,8 @@ urlpatterns = [
     path('vocabulary/', include('vocabulary.urls')),
     path('card-trainer/', include('card_trainer.urls')),
 ]
+# Serving the media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
