396d7747af637047f946c982d289c27b281a3608
vladiuse <vladiuse@gmail.com>
2025-02-28 14:30:15 +0300

    add main view

--- engCards/engCards/urls.py
+++ engCards/engCards/urls.py
@@ -3,10 +3,12 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('', views.index),
     path('vocabulary/', include('vocabulary.urls')),
     path('card-trainer/', include('card_trainer.urls')),
 ]
